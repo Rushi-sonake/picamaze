823cb4f740c5093cf5beecc14e09a91e30dd7ac6
Rushi-sonake <84307918+Rushi-sonake@users.noreply.github.com>
2022-06-22 12:56:00 +0530

    Update ShopifyApp.py

--- ShopifyApp.py
+++ ShopifyApp.py
@@ -25,6 +25,7 @@ class Shopify:
     def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
+        options.add_argument('--no-sandbox')
         options.add_experimental_option("detach", True)
         options.add_experimental_option("useAutomationExtension", False)
         options.add_experimental_option("excludeSwitches",["enable-automation"])
